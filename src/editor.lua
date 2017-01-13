9ec56dc615d2f8bf2ea2e2577cd7ca7abf79dbd0
Alban Linard <alban@linard.fr>
2017-01-13 14:40:20 +0100

    Add comments and fix temrination problems.

--- src/editor.lua
+++ src/editor.lua
@@ -4,6 +4,8 @@ local Layer = require "layeredata"
 math.randomseed (os.time ())
 
 return function ()
+
+  -- Classes for editor, client and server:
   local Editor = {}
   local Client = {}
   local Server = {}
@@ -11,63 +13,88 @@ return function ()
   Client.__index = Client
   Server.__index = Server
 
+  -- Create `editor`:
   local editor  = setmetatable ({}, Editor)
   local running = true
   local threads = {}
 
+  -- Create an empty layer:
   local function empty ()
     local layer = Layer.new {}
     layer [Layer.key.refines] = {}
     return layer
   end
 
+  -- Apply a `patch` to a `target` layer:
   local function apply (target, patch)
     local refines = target [Layer.key.refines]
-    if #refines > 0 then -- proxy
+    if #refines > 0 then
+      -- If `target` is a proxy, apply `patch` on it,
+      -- but write to its first refinement:
       Layer.write_to (target, refines [#refines])
       local ok, err = pcall (patch, target)
       Layer.write_to (target, nil)
       return ok, err
-    else -- model
+    else
+      -- If `target` is a model, apply `patch` on it.
       patch (target)
     end
   end
 
+  -- Push `layer` in the refinements of `target`:
   local function push (target, layer)
     local refines = target [Layer.key.refines]
     refines [#refines+1] = layer
   end
 
+  -- Pop `layer` from the refinements of `target`:
   local function pop (target, layer)
     local refines = target [Layer.key.refines]
     for i = #refines, 1, -1 do
       if refines [i] == layer then
-        table.remove (refines, i)
+        for j = i+1, #refines do
+          refines [j-1] = refines [j]
+        end
+        refines [#refines] = nil
         return
       end
     end
   end
 
+  -- Receive a message that matches `filter`:
   local function receive (to, filter)
+    -- `filter` is a function that takes the message contents as arguments,
+    -- and returns `true` if it is accepted:
     filter = filter or function () return true end
-    while running do
+    while running or #to.messages ~= 0 do
+      -- This sleep must be at the beginning, **not** at the end,
+      -- because it would not ensure that the last message is received.
       Copas.sleep (0)
       local message = to.messages [1]
       if message and filter (table.unpack (message)) then
         if math.random (10) > 5 then
+          -- Randomly sleep to create nondeterminism:
           Copas.sleep (0)
         end
         table.remove (to.messages, 1)
+        -- If `message` exists and matches `filter`, return its contents.
         return table.unpack (message)
       end
     end
   end
 
+  -- Send a message:
   local function send (to, ...)
     to.messages [#to.messages+1] = { ... }
-    Copas.sleep (0)
   end
 
+  -- Add the `send` and `receive` methods to clients and server:
+  Client.send    = send
+  Client.receive = receive
+  Server.send    = send
+  Server.receive = receive
+
+  -- Create the server:
   local server = setmetatable ({
     proxy    = empty (),
     model    = empty (),
@@ -75,8 +102,10 @@ return function ()
     messages = {},
   }, Server)
   push (server.proxy, server.model)
+  editor.server = server
 
-  function Client.new ()
+  -- Create a client:
+  function Editor.client ()
     local client = setmetatable ({
       proxy    = empty (),
       model    = empty (),
@@ -84,82 +113,75 @@ return function ()
     }, Client)
     push (client.proxy, client.model)
     server.clients [client] = true
+    -- Create a thread to wait for patches from other clients:
     Copas.addthread (function ()
-      while running do
-        client:update ()
+      while true do
+        local _, patch = client:receive (function (c, _) return c ~= client end)
+        if not patch then
+          return
+        end
+        apply (client.model, patch)
       end
     end)
     return client
   end
-  Client.send    = send
-  Client.receive = receive
-  Server.send    = send
-  Server.receive = receive
 
+  -- Apply a patch on a client:
   function Client:patch (patch)
     local co = Copas.addthread (function ()
-      -- create a new layer for the patch
+      -- Create a fresh layer to apply `patch`:
       local layer = empty ()
       push (self.proxy, layer)
-      -- apply the patch locally
+      -- Apply `patch` locally:
       local success = apply (self.proxy, patch)
       if success then
-        -- send the patch to the server
+        -- Send `patch` to the server:
         server:send (self, patch)
-        -- receive answer from the server
+        -- Receive the answer from the server:
         local _, _, ack = self:receive (function (_, p, _) return p == patch end)
         if ack then
-          -- insert the patch into the model
+          -- Apply `patch` to the model:
           apply (self.model, patch)
         end
       end
-      -- cleanup
+      -- Cleanup:
       pop (self.proxy, layer)
       threads [coroutine.running ()] = nil
       return success
     end)
+    -- Store the thread, required to detect termination:
     threads [co] = true
   end
 
-  function Client:update ()
-    local _, patch = self:receive (function (client, _) return client ~= self end)
-    if patch then
-      apply (self.model, patch)
-    end
-  end
-
-  function Server:handle ()
-    local origin, patch = self:receive ()
-    if not patch then
-      return
-    end
-    -- test the patch
-    local layer = empty ()
-    push (self.proxy, layer)
-    local success = apply (self.proxy, patch)
-    if success then
-      -- apply the patch
-      apply (self.model, patch)
-      -- send the successful patch to clients
-      for client in pairs (self.clients) do
-        client:send (origin, patch, success)
-      end
-    else
-      -- send failure message
-      origin:send (origin, patch, success)
-    end
-    -- cleanup
-    pop (self.proxy, layer)
-  end
-
+  -- Create a thread to perform the server loop:
   Copas.addthread (function ()
     while running do
-      server:handle ()
+      -- Receive a patch from any client:
+      local origin, patch = server:receive ()
+      if not patch then
+        return
+      end
+      -- Test the patch on a fresh layer:
+      local layer = empty ()
+      push (server.proxy, layer)
+      local success = apply (server.proxy, patch)
+      if success then
+        -- Apply the patch to the model:
+        apply (server.model, patch)
+        -- Send the successful patch to all clients:
+        for client in pairs (server.clients) do
+          client:send (origin, patch, success)
+        end
+      else
+        -- Send a failure message to the origin client:
+        origin:send (origin, patch, success)
+      end
+      -- Cleanup:
+      pop (server.proxy, layer)
     end
   end)
 
-  editor.server = server
-
+  -- Run the editor and perform actions described in the `f` function:
   function Editor.run (_, f)
     Copas.addthread (function ()
       running = true
@@ -169,6 +191,7 @@ return function ()
     Copas.loop ()
   end
 
+  -- Wait for edition to finish:
   function Editor.wait ()
     repeat
       Copas.sleep (0)
@@ -178,10 +201,10 @@ return function ()
       end
       finished = finished and #server.messages == 0
     until finished
-  end
-
-  function Editor.client ()
-    return Client.new ()
+    for client in pairs (server.clients) do
+      local refines = client.proxy [Layer.key.refines]
+      assert (#refines == 1)
+    end
   end
 
   return editor
--- src/editor_spec.lua
+++ src/editor_spec.lua
@@ -6,6 +6,8 @@ math.randomseed (os.time ())
 describe ("collaborative edition", function ()
 
   it ("works on simple example", function ()
+    -- This example creates two clients `c1` and `c2`.
+    -- They update the model by adding `1` to a field of the model.
     local editor = Editor ()
     local c1 = editor:client ()
     local c2 = editor:client ()
@@ -18,22 +20,31 @@ describe ("collaborative edition", function ()
       end)
       editor:wait ()
     end)
+    -- Check that the final value is `2` for the server model,
+    -- and for both clients:
     assert.are.equal (2         , editor.server.model.x)
     assert.are.equal (c1.model.x, editor.server.model.x)
     assert.are.equal (c2.model.x, editor.server.model.x)
+    assert.are.equal (c1.proxy.x, editor.server.model.x)
+    assert.are.equal (c2.proxy.x, editor.server.model.x)
   end)
 
   it ("is consistent between clients and server", function ()
+    -- This example creates several clients that apply several patches.
+    -- Each patch updates a specific field of the model with a random value.
     local editor    = Editor ()
     local clients   = {}
-    local nclients  = 2
-    local nmessages = 3
+    local nclients  = 5
+    local nmessages = 5
     for i = 1, nclients do
       clients [i] = editor:client ()
     end
     editor:run (function ()
       for _ = 1, nmessages do
         for _, client in ipairs (clients) do
+          -- We cannot put the `math.random` call within the patch,
+          -- as its result would take different values in the server
+          -- and the clients.
           local value = math.random (1000)
           client:patch (function (model)
             model.value = value
@@ -42,12 +53,19 @@ describe ("collaborative edition", function ()
       end
       editor:wait ()
     end)
+    -- Check that the server and all clients agree on the final value:
     for _, client in ipairs (clients) do
       assert.are.equal (client.model.value, editor.server.model.value)
+      assert.are.equal (client.proxy.value, editor.server.model.value)
     end
   end)
 
   it ("is consistent with patch order", function ()
+    -- This example creates several clients, that apply several patches.
+    -- The model stores several stacks, one for each client.
+    -- Each patch pushes a random value to the stack corresponding
+    -- to its client in the model.
+    -- The sequence of patches applied from a client should be preserved.
     local editor    = Editor ()
     local clients   = {}
     local nclients  = 5
@@ -66,6 +84,9 @@ describe ("collaborative edition", function ()
       end
       for _ = 1, nmessages do
         for i, client in ipairs (clients) do
+          -- We cannot put the `math.random` call within the patch,
+          -- as its result would take different values in the server
+          -- and the clients.
           local value   = math.random (1000)
           local cvalues = values [client]
           cvalues [#cvalues+1] = value
@@ -77,6 +98,7 @@ describe ("collaborative edition", function ()
       end
       editor:wait ()
     end)
+    -- Check that the sequence of patches is preserved for each client:
     for i, client in ipairs (clients) do
       local cvalues = values [client]
       for j, value in ipairs (client.model [i]) do
